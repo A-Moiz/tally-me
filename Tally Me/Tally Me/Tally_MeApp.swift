8c34114e953f4d98bea9b7b1a953c24df503ed2d
Abdul Moiz <abdul.m01z10123@gmail.com>
2024-06-15 18:13:23 +0100

    Created Xcode project

---
+++ Tally Me/Tally Me/Tally_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tally_MeApp.swift
+//  Tally Me
+//
+//  Created by Abdul Moiz on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tally_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
